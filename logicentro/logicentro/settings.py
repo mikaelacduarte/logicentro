5459bd148c98d57eabead3d62e4f31a65158d443
DanielRezez <danielrezes@outlook.com>
2024-10-18 22:55:22 -0300

    inicializando integração - servidor nao esta rodando

--- logicentro/logicentro/settings.py
+++ logicentro/logicentro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'veiculo',
     'empresa',
     'motorista',
@@ -47,13 +48,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'logicentro.urls'
@@ -90,10 +93,12 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbtransportes',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # ou o IP do servidor
+        'PORT': '3306',
 }
 
 
@@ -114,6 +119,11 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"  #permite que o domínio do localhost faça requisições ao servidor
+]
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Internationalization
